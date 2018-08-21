cc2477d46aa7ca262052651f0f2d8d72ebe52640
Pulkit gera <pulkithifi@yahoo.com>
2018-08-21 23:07:44 +0530

    background made

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Screen:
     
     def draw(self):
         for base in self.screen:
-            for stone in base[10:-10]:
+            for stone in base[20:-10]:
                 print(stone, end=" ")
             print(' ')
 
@@ -55,65 +55,74 @@ class Board(Screen):
     def draw(self,surface):
         surface.screen[-3:] = self._board_base_char
 
-    def bridge(self,surface):
-        surface.screen[-7][screen.screen_width-12:screen.screen_width-10] = self._board_bridge_char
-        surface.screen[-6][screen.screen_width-12:screen.screen_width-10] = self._board_bridge_char
-        surface.screen[-7][screen.screen_width-10:screen.screen_width-8] = '?'
-        surface.screen[-6][screen.screen_width-10:screen.screen_width-8] = '?'
-        surface.screen[-7][screen.screen_width-8:screen.screen_width-6] = self._board_bridge_char
-        surface.screen[-6][screen.screen_width-8:screen.screen_width-6] = self._board_bridge_char
-        surface.screen[-7][screen.screen_width-6:screen.screen_width-4] = '?'
-        surface.screen[-6][screen.screen_width-6:screen.screen_width-4] = '?'
+    def bridge(self,surface,start):
+        x = random.randint(0,1)
+        if np.all(surface.screen[12+x:17,start-6:start+6] == ' '):
+            surface.screen[12+x:14,start:start+1] = 'x'
+            surface.screen[12+x:14,start+1:start+2] = '?'
+            surface.screen[12+x:14,start+2:start+3] = 'x' 
+        pass
+    
     def pit(self,surface):
-        surface.screen[-3:,45:48] = ' '
+        if np.all(surface.screen[-3:42:46]=='#') and np.all(surface.screen[-3:49:52] =='#'):
+            surface.screen[-3:,46:52] = ' '
+    
+    def large_pit(self,surface):
+        if np.all(surface.screen[-3:42:44]=='#') and np.all(surface.screen[-3:49:51] =='#'):
+            surface.screen[-3:,46:49] = ' '
     
     def clouds(self,surface,x_max,y_start,y_end):
         x = random.randint(x_max-3,x_max)
         y = random.randint(y_start,y_end)
-        if surface.screen[x-1][y-4] == ' ':
+        if np.all(surface.screen[x-3:x+1,y-4:y+1] == ' '):
             surface.screen[x-1][y-4]='('
-        if surface.screen[x-2][y-3] == ' ':
             surface.screen[x-2][y-3]='/'
-        if surface.screen[x-3][y-2] == ' ':
             surface.screen[x-3][y-2]='-'
-        if surface.screen[x-2][y-1] == ' ':
             surface.screen[x-2][y-1]='\\'
-        if surface.screen[x-1][y] == ' ':
-            surface.screen[x-1][y]=')'
-        if surface.screen[x][y-3] == ' ' and surface.screen[x][y-2] == ' ' and surface.screen[x][y-1] == ' ':
+            if surface.screen[x-1][y+1] == ' ':
+                surface.screen[x-1][y]=')'
             surface.screen[x][y-3:y]='-'
 
     def pipe(self,surface,y_start):
-        choice = random.randint(0,3)
-        surface.screen[12+choice:17,y_start:y_start+3]="8"
+        choice = random.randint(0,2)
+        if np.all(surface.screen[13+choice:17,y_start-10:y_start+10]==' '):
+            surface.screen[12+choice:17,y_start:y_start+3]="8"
 
+    def nothing(self,surface):
+        #surface.screen[14:17,55:59] = ' '
+        pass
 
 def make_scene(screen,board):
     helpers.clear()
     helpers.clear()
     print(Fore.RED+"                         Mario               "+Style.RESET_ALL)
-    #board.draw(screen)
-    screen.draw()
-    
+    screen.draw() 
             
-
-
-
 screen = Screen()
 board = Board()
-board.draw(screen)
-board.bridge(screen)
-board.pipe(screen,20)
-board.pit(screen)
-for i in range(0,10):
+def initScreen(screen,board):
+    #player to be added
+    board.draw(screen)
+    for i in range(0,10):
+        board.clouds(screen,8,25,39)
 
-    board.clouds(screen,8,15,39)
+initScreen(screen,board)
+board.bridge(screen,30)
+board.pipe(screen,15)
+board.pit(screen)   
+def createObstacle(board,screen):
+    board.clouds(screen,8,50,58)
+    options = [board.pipe(screen,54),board.pit(screen),board.bridge(screen,56)]
+    choice = random.randint(0,2)
+    return options[choice]
 
 make_scene(screen,board)
 count = 0
-
+iter =0
 while True:
-    
+    iter +=1 
+    if iter % 5 == 0:
+        createObstacle(board,screen)
     try:
         keypress = input.get_input()
 
