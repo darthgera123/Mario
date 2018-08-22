b46017eeeed0b45d3407d603e73a0cc7518cc75a
Pulkit gera <pulkithifi@yahoo.com>
2018-08-23 04:43:35 +0530

    Player and coins working

--- helpers.py
+++ helpers.py
@@ -1,7 +1,9 @@
 from os import name
 from os import system
 import colorama
+import random
 from colorama import Fore,Style
+
 colorama.init()
 
 def clear():
@@ -15,16 +17,24 @@ def clear():
         _ = system('clear')
 
 
-def test(screen):
-    clear()
-    clear()
-    print(Fore.RED+"                         Mario               "+Style.RESET_ALL)
-    screen.draw()
 
 
-def make_scene(screen):
-    clear()
-    clear()
-    print(Fore.RED+"                         Mario               "+Style.RESET_ALL)
-    screen.draw()
     
+def make_coins(screen,y,coin_count):
+    
+    for i in range(0,2): 
+        x_coins = random.randint(13,16)
+        y_coins = random.randint(y,y+20)  
+        if screen.screen[x_coins][y_coins] == ' ' and coin_count <= 5:
+            screen.screen[x_coins][y_coins] = 'c'
+            coin_count += 1
+        return coin_count
+def eat_coins(screen,player):
+    x_player = player.retx()
+    y_player = player.rety()
+    options = [screen.screen[x_player][y_player+1],screen.screen[x_player][y_player-2],screen.screen[x_player-1][y_player-2],screen.screen[x_player-1][y_player+1],\
+               screen.screen[x_player-2][y_player-2],screen.screen[x_player-1][y_player+1]]
+    for o in options:
+        if  o== 'c':
+            return 1
+    return 0      
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from colorama import Fore, Back, Style
 import random
 import input
 from constants import terminal_ht, terminal_width
-from helpers import make_scene, test
+from helpers import  make_coins,eat_coins,clear
 import constants
 import time
 from background import Screen
@@ -21,8 +21,8 @@ colorama.init()
 
 class Player():
     
-    def __init__(self):
-        self.x = 16
+    def __init__(self,x):
+        self.x = x
         self.y = 24
     
     def draw(self,surface,dir=0):
@@ -56,7 +56,16 @@ class Player():
             surface.screen[self.x][self.y-1]= ' '
             surface.screen[self.x-1][self.y]= ' '
             surface.screen[self.x-1][self.y-1]= ' '
-            self.x -=2
+            if surface.screen[self.x+1][self.y-1] == '?' or surface.screen[self.x+1][self.y] == '?':
+                self.x -=5
+            if surface.screen[self.x+1][self.y-1] == '#' or surface.screen[self.x+1][self.y] == '#':
+                self.x -=3
+            if surface.screen[self.x+1][self.y-1] == ' ' or surface.screen[self.x+1][self.y] == ' ':
+                self.x -=1
+            if surface.screen[self.x+1][self.y-1] == 'x' or surface.screen[self.x+1][self.y] == 'x':
+                self.x -=2
+            if surface.screen[self.x+1][self.y-1] == '8' or surface.screen[self.x+1][self.y] == '8':
+                self.x -=4
         else:
             pass
 
@@ -96,28 +105,38 @@ class Player():
         return self.x
     def rety(self):
         return self.y
+    def setx(self,x):
+        self.x = x
+    def sety(self,y):
+        self.y = y
 
 screen = Screen()
 board = Board()
 enemyList = []
-player = Player()
+player = Player(16)
+health = 3
+count = 0
 
+def make_scene(screen):
+    clear()
+    clear()
+    print(Fore.RED+"                         Mario               "+Style.RESET_ALL)
+    print(Fore.GREEN+"  Health               "+str(health)+Style.RESET_ALL+Fore.YELLOW+"   Score             "+str(count*10)+Style.RESET_ALL)
+    screen.draw()
 
-def initScreen(screen,board):
-  
+def initScreen(screen,board): 
     player.draw(screen)
-    board.pipe(screen,35)
+    board.bridge(screen,35)
     board.draw(screen)
     for i in range(0,10):
         board.clouds(screen,8,25,39)
 
 initScreen(screen,board) 
 def createObstacle(board,screen):
-    board.clouds(screen,8,50,58)
+    board.clouds(screen,6,50,58)
     options = [board.pipe(screen,54),board.pit(screen),board.bridge(screen,56)]
     choice = random.randint(0,2)
     return options[choice]
-
 
 
 def checkMove(screen,player):
@@ -125,34 +144,32 @@ def checkMove(screen,player):
     y = player.rety()
     if screen.screen[x][y+1] == 'O':
         return 1
-    elif screen.screen[x][y+1] == '8' or screen.screen[x][y+1] == 'x' or screen.screen[x][y+1] == 'x':
+    elif screen.screen[x][y+1] == '8' or screen.screen[x][y+1] == 'x' or screen.screen[x][y+1] == 'x' or screen.screen[x-1][y+1] == 'x' or screen.screen[x-1][y+1] == 'x'\
+            or screen.screen[x][y+1] == '#':
         return 2
-    elif screen.screen[x][y-2] == '8' or screen.screen[x][y-2] == 'x' or screen.screen[x][y-2] == 'x':
+    elif screen.screen[x][y-2] == '8' or screen.screen[x][y-2] == 'x' or screen.screen[x][y-2] == 'x' or screen.screen[x-1][y-2] == 'x' or screen.screen[x-1][y-2] == 'x' \
+            or screen.screen[x][y-2] == '8':
         return 3
-    elif screen.screen[x-2][y] == '?':
-        screen.screen[x-2][y] = 'x'
+    elif screen.screen[x-2][y] == '?' or screen.screen[x-2][y-1] == '?':
+        screen.screen[x-2][y] = ' '       
         return 4
-coin_count = int(0)
-def make_coins(screen,y,coin_count):
-    
-    for i in range(0,5): 
-        x_coins = random.randint(13,16)
-        y_coins = random.randint(y,y+20)  
-        if screen.screen[x_coins][y_coins] == ' ' and coin_count <= 10:
-            screen.screen[x_coins][y_coins] = 'c'
-            coin_count += 1
-        return coin_count
-def eat_coins(screen,player):
-    x_player = player.retx()
-    y_player = player.rety()
-    options = [screen.screen[x_player][y_player+1],screen.screen[x_player][y_player-2],screen.screen[x_player-1][y_player-2],screen.screen[x_player-1][y_player+1],\
-               screen.screen[x_player-2][y_player-2],screen.screen[x_player-1][y_player+1]]
-    for o in options:
-        if  o== 'c':
-            return 1
-    return 0      
 
-count = 0
+def restart(screen,player):
+    x = player.retx()
+    y = player.rety()
+    screen.screen[x][y] = ' '
+    screen.screen[x-1][y] = ' '
+    screen.screen[x][y-1] = ' '
+    screen.screen[x-1][y-1] = ' '
+    
+    del(player)
+    player = Player(14)
+    return player
+
+
+
+coin_count = int(0)
+
 iter =0
 dir =0
 
@@ -164,11 +181,11 @@ while True:
 
     coin_count = make_coins(screen,38,coin_count)     
     if iter %2 == 0:
-        try:
-            player.fall(screen)
-        except:
-            player.clean(screen)
-    
+        if player.retx() + 1 == 20:
+            player = restart(screen,player)
+            health -= 1
+            
+        player.fall(screen)
     if eat_coins(screen,player):
         count += 1
         coin_count -= 1 
@@ -180,12 +197,16 @@ while True:
             dir=0
             move = checkMove(screen,player)
             if move != 2 and move != 1:
-                screen.move_right()  
+                screen.move_right()
+            if move == 4:
+                count +=1  
         elif keypress == 'a':
             dir=1
             move = checkMove(screen,player)
             if move != 3 and move!= 1:
                 screen.move_left()
+            if move == 4:
+                count +=1
         elif keypress == 'w':
             player.jump(screen)
         elif keypress == 'q':
